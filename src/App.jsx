dd2248f56930b65d103fd5b6faa4289ed478f324
OSE233 <semilore.olatunji123@gmail.com>
2026-08-29 14:24:22 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import AnimeDiary from "./components/AnimeDiary";
+import './App.css'
+const App =() =>{
+  return(
+    <div>
+      <AnimeDiary />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AnimeDiary.jsx
@@ -0,0 +1,88 @@
+
+const AnimeDiary=() =>{
+  return(
+    <div>
+      
+
+    <h1>
+        Hello and welcome to my anime diary.
+    </h1>
+    <p>Oh sorry i didn't introduce myself,<br/>
+      I'm Olatunji Oluwasemilore Emerald <br/>
+      Temitope Tolulope Mayomikun and some<br/>
+       other names.So here r the animes i <br/>
+       watched in year 2025 enjoy .  
+    </p> 
+
+    <h1>DAN DA DAN SEASON 2  </h1>
+<img src="pics/ddd.jpg" alt="dan da dan"/>
+<p>
+    This is one of my fav its came out last two years. Its an intresting anime about a girl<br/>
+     named momo and guy named okarun who believe in superstitions like aliens and ghost, <br/>
+     anyways there real and they r hunting both of them. so they get other ppl entangled.   <br/>
+     Jiji, Airia and Momos grandma seiko theres also this dunmass guy idk his name.
+ 
+</p>
+
+
+
+
+ <h1> MY HEROS ACADEMIA / BOKU NO HERO DESU (MHA/BHN)</h1>
+<img src="pics/mha.jpg" alt="MHA/BKN"/>
+<p>
+    This one is about a guy Midoriya Izuku who was born powerless and often bullied by some guy<br/>
+    Katsuki Bakugo. Until one day he was acknowledged by the no.1 hero who passed down a powerful<br/>
+    power to him and now hes on a quest to destroy the creator of this power with the help of his<br/>
+     set mates and pro heros.
+</p>
+
+
+
+ <h1> SCISSORS SEVEN SEASON 5    </h1>
+<img src="pics/scissors.jpg" alt="SS"/>
+<p>
+    This is bout a funny and stupid guy seven who just wants to be rich but his boss gave him a job as an <br/>
+    assaisain so he find out he was actually an assaisain but he cant remember he needs to find out bout<br/>
+    his past before hes killed.
+
+</p>
+
+
+
+ <h1> SHAMEN KING</h1>
+<img src="pics/sh.jpg" alt="SH"/>
+<p>
+    i dont really know much bout this but some guy Yho just wants to be the king so he can live a comfortable<br/>
+    life so in order to do that he needs to fight and prove that hes worthy and he has to fight a demon who<br/>
+    constantly reincarnate just to be the king.
+</p>
+
+
+
+
+ <h1>THE FRAGRANT FLOWER BLOSSOMS WITH DIGNITY </h1>
+<img src="pics/flbd.jpg" alt="TFFBD"/>
+<p>
+    no much to say this is a bakers son Rintaro but he dosent want anyone to know about it so he hides it<br/>
+    until some girl Karuko Waguri from another school finds out they become friends even there friends r friends
+</p>
+
+
+
+<h1>TOILET BOUND HONAKOKUN /TBH</h1>
+<img src="pics/tbh.png" alt="TBH"/>
+<p>
+This is another one of my no.1 fav so a girl Yashiro Nene goes to a school that is haunted by seven spirits<br/>
+One of them she is bound to him Honako/Amane he used to be alive until one day he killed himself and his <br/>
+twin brother so they have to find out the traitor who is trying to make those seven spirts disappears.
+
+</p>
+
+
+
+
+    </div>
+  )
+}
+
+export default AnimeDiary
